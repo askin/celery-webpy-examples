170bc1e0cf63ea41bd8524d1a7ff63adcb9ee12a
Aşkın Yollu <askin@askin.ws>
2013-02-16 16:29:18 +0200

    use celery on web service (webservice -> celery)

--- webpy/services.py
+++ webpy/services.py
@@ -1,4 +1,10 @@
+import sys
 import web
+sys.path.append("../celery")
+from tasks import celeryAdd
+from tasks import celeryDiff
+from tasks import celeryMultiple
+from tasks import celerySquare
 
 
 class service:
@@ -34,6 +40,44 @@ class service:
     def square(self, arg):
         return arg * arg
 
+class CeleryService:
+    def GET(self, op, arg1, arg2=None):
+        template = '{"status" : "success", "retval" : %s}'
+        failure = '{"status" : "failure", "reason" : "%s"}'
+
+        try:
+            if op == 'add':
+                rt = template % self.add(int(arg1), int(arg2))
+            elif op == 'diff':
+                rt = template % self.diff(int(arg1), int(arg2))
+            elif op == 'multiple':
+                rt = template % self.multiple(int(arg1), int(arg2))
+            elif op == 'square':
+                rt = template % self.square(int(arg1))
+            else:
+                rt = failure % 'unknown op'
+        except TypeError, e:
+            rt = failure % e
+
+        return rt
+
+    def add(self, arg1, arg2):
+        # if you don't want to result, remove .get()
+        return celeryAdd.delay(arg1, arg2).get()
+
+    def diff(self, arg1, arg2):
+        # if you don't want to result, remove .get()
+        return celeryDiff.delay(arg1, arg2).get()
+
+    def multiple(self, arg1, arg2):
+        # if you don't want to result, remove .get()
+        return celeryMultiple.delay(arg1, arg2).get()
+
+    def square(self, arg):
+        # if you don't want to result, remove .get()
+        return celerySquare.delay(arg).get()
+        return arg * arg
+
 
 class index:
     def GET(self):
@@ -44,6 +88,8 @@ if __name__ == "__main__":
     urls = (
         '/service/([a-z]*)/(\d+)/(\d+)', 'service',
         '/service/([a-z]*)/(\d+)', 'service',
+        '/celery-service/([a-z]*)/(\d+)/(\d+)', 'CeleryService',
+        '/celery-service/([a-z]*)/(\d+)', 'CeleryService',
         '/', 'index'
         )
     app = web.application(urls, globals())
